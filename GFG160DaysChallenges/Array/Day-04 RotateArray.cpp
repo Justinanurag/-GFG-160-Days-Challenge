ba954d8b1eeb6f3ecd57d282ca98f3e0cb2b6f1f
justinanurag0.2 <142174616+Justinanurag@users.noreply.github.com>
2025-04-14 22:08:38 +0530

    Add files via upload
    Date: 14/04/2025 
    Rotate Array

---
+++ GFG160DaysChallenges/Array/Day-04 RotateArray.cpp
@@ -0,0 +1,66 @@
+//Approach 01: Using Reversal Algorithm
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function to rotate an array by d elements to the left
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+  
+    // Handle the case where d > size of array
+    d %= n;
+  
+    // Reverse the first d elements
+    reverse(arr.begin(), arr.begin() + d);
+
+    // Reverse the remaining n-d elements
+    reverse(arr.begin() + d, arr.end());
+  
+    // Reverse the entire array
+    reverse(arr.begin(), arr.end());
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 2;
+    
+  	rotateArr(arr, d);
+  
+    for (int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
+
+//Approach 02: Rotate one by one
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to left rotate array by d positions
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+  
+    // Repeat the rotation d times
+    for (int i = 0; i < d; i++) {
+      
+        // Left rotate the array by one position
+        int first = arr[0];
+        for (int j = 0; j < n - 1; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr[n - 1] = first;
+    }
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 2;
+
+    rotateArr(arr, d);
+
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
